2e179ce0764f2d853da139461a4da46e9852ebff
SteRoa <steroagna66@gmail.com>
2018-06-14 00:20:40 +0200

    aggiunto proprietario alle posizioni

--- src/app/position.ts
+++ src/app/position.ts
@@ -3,11 +3,13 @@ export class Position {
     latitude: number = undefined;
     longitude: number = undefined;
     timestamp: number = undefined;
+    userId: string = undefined;
 
-    constructor (id?: number, latitude?: number, longitude?: number, timestamp?: number) {
+    constructor (id?: number, latitude?: number, longitude?: number, timestamp?: number, userId?: string) {
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
         this.timestamp = timestamp;
+        this.userId = userId;
     }
 }
